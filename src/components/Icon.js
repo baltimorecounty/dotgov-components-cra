ee9b82b30d8f8080d56f0a985aa62d50c1bf3030
martypowell <jpowel25@gmail.com>
2019-04-04 12:47:33 -0400

    fixed classname

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Icon = props => <i class={`far ${props.iconClass}`} />;
+const Icon = props => <i className={`far ${props.iconClass}`} />;
 
 Icon.propTypes = {
   /** The "fa" class for font-awesome pro, ex: "fa-hamburger" */
