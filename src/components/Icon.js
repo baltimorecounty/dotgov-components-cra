a77c9f191593382a3dc159d67dbdad99a3368975
martypowell <jpowel25@gmail.com>
2019-04-04 12:42:55 -0400

    added icon component and tests

---
+++ src/components/Icon.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Icon = props => <i class={`far ${props.iconClass}`} />;
+
+Icon.propTypes = {
+  /** The "fa" class for font-awesome pro, ex: "fa-hamburger" */
+  type: PropTypes.string.isRequired
+};
+
+export default Icon;
