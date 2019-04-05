08fb4863bdb954ba87fd2e032b725d121be55701
Marty Powell <jpowel25@gmail.com>
2019-04-04 20:51:52 -0400

    cahnged props to match fontawesoem componet

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,11 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons"; /** https://fontawesome.com/how-to-use/with-the-api/setup/importing-icons#packages */
 
-const Icon = props => <i className={`far ${props.iconClass}`} />;
+library.add(fas);
+
+const Icon = props => <FontAwesomeIcon {...props} />;
 
 Icon.propTypes = {
-  /** The "fa" class for font-awesome pro, ex: "fa-hamburger" */
-  type: PropTypes.string.isRequired
+  /** The "fa" class for font-awesome pro, ex: "hamburger" */
+  icon: PropTypes.string.isRequired
 };
 
 export default Icon;
