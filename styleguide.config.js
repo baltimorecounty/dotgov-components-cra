f1b3da82bc81dfbcb2efbf28c6c09cb4a8a0c282
Marty Powell <jpowel25@gmail.com>
2019-04-04 21:30:57 -0400

    implmeneted workaround for building

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,5 +2,8 @@ const path = require("path");
 module.exports = {
   ignore: ["**/components/**/*.test.js", "**/components/**/index.js"],
   usageMode: "expand",
-  require: [path.join(__dirname, "./src/App.scss")]
+  require: [path.join(__dirname, "./src/App.scss")],
+  webpackConfig: require("react-scripts/config/webpack.config")(
+    "development"
+  ) /** Workaround - https://github.com/styleguidist/react-styleguidist/issues/1247 */
 };
