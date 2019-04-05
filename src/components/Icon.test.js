d01354663e8c8637099bb0c9f7626f4ef06f5de5
Marty Powell <jpowel25@gmail.com>
2019-04-04 20:51:25 -0400

    fixed icon tests

--- src/components/Icon.test.js
+++ src/components/Icon.test.js
@@ -2,8 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Icon from "./Icon";
 
-it("renders without crashing", () => {
+const spy = jest.spyOn(global.console, "error");
+
+it("renders free icon without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Icon type="fa-hamburger" />, div);
+  ReactDOM.render(<Icon icon="hamburger" />, div);
   ReactDOM.unmountComponentAtNode(div);
+
+  expect(spy).not.toHaveBeenCalled(); // react-fontawesome will throw an error if the icon doesn't exist
+});
+
+it("renders pro icon without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Icon icon="acorn" />, div);
+  ReactDOM.unmountComponentAtNode(div);
+
+  expect(spy).not.toHaveBeenCalled(); // react-fontawesome will throw an error if the icon doesn't exist
 });
