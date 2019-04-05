75de197d23f8083f61d2d5d255911e17fd1c9745
Marty Powell <jpowel25@gmail.com>
2019-04-04 21:17:51 -0400

    changed to use the sass file

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,5 +2,5 @@ const path = require("path");
 module.exports = {
   ignore: ["**/components/**/*.test.js", "**/components/**/index.js"],
   usageMode: "expand",
-  require: [path.join(__dirname, "./src/App.css")]
+  require: [path.join(__dirname, "./src/App.scss")]
 };
