e7c2b2780243f838d31ac690dd50eee395f551f0
Marty Powell <jpowel25@gmail.com>
2019-04-10 19:57:21 -0400

    fixed test

--- src/components/Form/TextArea.test.js
+++ src/components/Form/TextArea.test.js
@@ -4,6 +4,9 @@ import TextArea from "./TextArea";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<TextArea label="Describe your problem" />, div);
+  ReactDOM.render(
+    <TextArea id="problem-description" label="Describe your problem" />,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
