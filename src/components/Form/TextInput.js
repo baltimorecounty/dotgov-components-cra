b5f8853f66ff7a95b83a67c7cdd284af365045f8
Marty Powell <jpowel25@gmail.com>
2019-04-10 12:53:05 -0400

    adjsuted font-weight

--- src/components/Form/TextInput.js
+++ src/components/Form/TextInput.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import { Icon } from "../";
 
 const TextInput = props => {
+  const [error, setErrors] = useState("");
   const { id, hint, label } = props;
+
+  useEffect(() => {
+    setErrors("Enter your full name");
+  });
+
   return (
     <div className="form-field">
       <label for={id} className="dg_label">
         <span className="dg_label-text">{label}</span>
         {hint && <span className="dg_label-hint">{hint}</span>}
+        {error && (
+          <span className="dg_label-validation">
+            <Icon icon="exclamation-triangle" /> {error}
+          </span>
+        )}
       </label>
-
       <input id={id} className="form-field_input--text" {...props} />
     </div>
   );
