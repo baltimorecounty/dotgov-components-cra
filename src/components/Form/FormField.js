b3c5e2d2cac7d7e684e14dafad0a5fc9562e4f13
Marty Powell <jpowel25@gmail.com>
2019-04-10 20:05:41 -0400

    updated component

--- src/components/Form/FormField.js
+++ src/components/Form/FormField.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Icon } from "../";
+import { Icon } from "..";
 
 const FormField = props => {
   const { id, label, hint, error } = props;
