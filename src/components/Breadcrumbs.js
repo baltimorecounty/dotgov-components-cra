5ae054759f498771745a7b5df4e17a6ffae1d521
Marty Powell <jpowel25@gmail.com>
2019-04-15 20:46:55 -0400

    added breadcrumb comopnet

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -1,16 +1,46 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const Breadcrumb = props => {
+  const { item, index, arr } = props;
+  const isLastItem = index === arr.length - 1;
+  const link = isLastItem ? (
+    <span itemprop="name">{item.text}</span>
+  ) : (
+    <a itemprop="item" href={item.url}>
+      <span itemprop="name">{item.text}</span>
+    </a>
+  );
+  return (
+    <li
+      className={"dg_breadcrumb"}
+      itemprop="itemListElement"
+      itemscope
+      itemtype="http://schema.org/ListItem"
+    >
+      {link}
+      <meta itemprop="position" content={index + 1} />
+    </li>
+  );
+};
+
 const Breadcrumbs = props => {
   const { items } = props;
   return (
-    <div className="dg_breadcrumbs">
-      <p>BreadCrumbs Go Here {items.length}</p>
-    </div>
+    <ol
+      className="dg_breadcrumbs"
+      itemscope
+      itemtype="https://schema.org/BreadcrumbList"
+    >
+      {items.map((item, index, arr) => (
+        <Breadcrumb item={item} index={index} arr={arr} />
+      ))}
+    </ol>
   );
 };
 
 Breadcrumbs.propTypes = {
+  /** A list of links containing the text for the link and the url for the link. */
   items: PropTypes.array.isRequired
 };
 
