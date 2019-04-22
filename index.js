5651f5c866af1546323a9db7b51c9b79574a2fcc
martypowell <jpowel25@gmail.com>
2019-04-22 10:43:59 -0400

    updates

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-export { default as Components } from "./src/components";
+export * from "./src/components";
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
 export { default as Icon } from "./Icon";
 export { default as IconButton } from "./IconButton";
+
+/** Forms */
+export { default as TextArea } from "./Form/TextArea";
+export { default as TextInput } from "./Form/TextInput";
